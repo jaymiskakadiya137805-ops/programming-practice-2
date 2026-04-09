558b9e4021e9aaf6579d72ee4fe748da791ff75c
jaymiskakadiya137805-ops <jaymis.kakadiya137805@marwadiuniversity.ac.in>
2026-04-09 15:36:21 +0530

    Add C program to calculate file size

---
+++ UNIT-4/11.C
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    FILE *fp;
+    int size;
+
+    fp = fopen("file1.txt", "r");
+
+    fseek(fp, 0, SEEK_END);
+    size = ftell(fp);
+
+    printf("File size: %d bytes", size);
+
+    fclose(fp);
+    return 0;
+}

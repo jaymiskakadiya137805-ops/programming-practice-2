9a1651fdc686365a87999abe7e7603d4198b795d
jaymiskakadiya137805-ops <jaymis.kakadiya137805@marwadiuniversity.ac.in>
2026-04-09 15:35:29 +0530

    Create 9.C

---
+++ UNIT-4/9.C
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <ctype.h>
+
+int main() {
+    FILE *fp;
+    char ch;
+    int upper=0, lower=0, digit=0, vowel=0, special=0;
+
+    fp = fopen("file1.txt", "r");
+
+    while ((ch = fgetc(fp)) != EOF) {
+        if (isupper(ch)) upper++;
+        else if (islower(ch)) {
+            lower++;
+            if (ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u')
+                vowel++;
+        }
+        else if (isdigit(ch)) digit++;
+        else special++;
+    }
+
+    printf("Upper: %d Lower: %d Digits: %d Vowels: %d Special: %d",
+           upper, lower, digit, vowel, special);
+
+    fclose(fp);
+    return 0;
+}
